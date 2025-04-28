1fd33467258271ccc675bd6acabb70a27aa48f32
zeyu <wangzeyu654@gmail.com>
2025-04-28 15:16:36 +0800

    ini

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {Button} from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>create pages as you like in the pages dir</h1>
+        <Button>bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
