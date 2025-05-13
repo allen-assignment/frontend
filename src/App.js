702901bbc9d87e4c95d6f316cd53887678f8e7b7
zeyu <wangzeyu654@gmail.com>
2025-05-13 22:12:52 +0800

    Cleaned up login with final version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import {Button} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
+import Login from "./pages/login/Login";
+import {useState} from "react";
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <h1>create pages as you like in the pages dir</h1>
-        <Button>bootstrap</Button>
-    </div>
-  );
+    const [isLogin, setIsLogin] = useState(true);
+
+    return (
+        <Container fluid className="auth-bg d-flex justify-content-center align-items-center">
+            <Login isLogin={isLogin} onToggle={() => setIsLogin(!isLogin)}/>
+        </Container>
+    );
 }
 
 export default App;
